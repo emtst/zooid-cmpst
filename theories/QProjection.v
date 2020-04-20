c3276ff2c05e8a24535d10cfc2718bed41fe97a7
Lorenzo Gheri <lorenzo.gheri@yahoo.it>
2020-04-20 11:10:59 +0200

    added new file QProjection.v and moved something

---
+++ theories/QProjection.v
@@ -0,0 +1,160 @@
+From mathcomp.ssreflect Require Import all_ssreflect seq.
+From mathcomp Require Import finmap.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Import Prenex Implicits.
+
+From Paco Require Import paco paco2.
+
+Require Import MPST.Atom.
+Require Import MPST.AtomSets.
+Require Import MPST.Forall.
+Require Import MPST.LNVar.
+Require Import MPST.Global.
+Require Import MPST.Local.
+Require Import MPST.Actions.
+Require Import MPST.Projection.
+Require Import MPST.WellFormed.
+
+
+Section QProjection.
+
+(*next Variable and Definition may be useless*)
+Variable PAR : {fset role}.
+
+Definition PAR2 := (PAR `*` PAR)%fset.
+
+Open Scope fmap.
+
+  Definition qproj_rel := rg_ty -> {fmap role * role -> seq (lbl * mty) } -> Prop.
+  Inductive qProj_ (r : qproj_rel) : qproj_rel :=
+  | qprj_end : qProj_ r rg_end [fmap]%fmap(*[fmap qq:PAR2 => [::]]*)
+  | qprj_none p p' CONT Q:
+      p != p' -> 
+      (forall l Ty G, CONT l = Some (Ty, G) -> r G Q) ->
+      qProj_ r (rg_msg None p p' CONT) Q
+  | qprj_some p p' CONT l Ty G Q Q':
+      p != p' -> CONT l = Some (Ty, G) ->
+      deq Q' (p, p') == Some ((l, Ty), Q) -> r G Q ->
+      qProj_ r (rg_msg (Some l) p p' CONT) Q'
+  .
+  Hint Constructors qProj_.
+  Lemma qProj_monotone : monotone2 qProj_.
+  Proof.
+  move=> x0 x1 r r' it LE; move: it; case=>//.
+  + move=> p p' CONT Q neq hp.
+    apply: (qprj_none neq); move=> l Ty G CONTeq; apply LE.
+    by apply: (hp l Ty G).
+  + move=> p p' CONT l Ty G Q Q' neq CONTeq enqu rel.
+    apply: (qprj_some neq CONTeq enqu); by apply (LE _ _ rel).
+  Qed.
+  Hint Resolve qProj_monotone.
+  Definition qProject CG Q := paco2 (qProj_) bot2 CG Q.
+
+
+
+(*L to D and F; about the next 6 lemmas:
+  - should be moved
+  - might be be turned into 2 or even 0
+*)
+
+  Lemma qProject_end_inv_aux Q GG: 
+    GG = rg_end -> qProject GG Q -> 
+    Q = ([fmap]%fmap).
+  Proof.
+  move=> eq hp; punfold hp.
+  move: hp eq => [||]//= [].
+  Qed.
+
+  Lemma qProject_end_inv Q:
+  qProject rg_end Q -> 
+    Q = ([fmap]%fmap).
+  Proof.
+  by apply qProject_end_inv_aux.
+  Qed.
+
+
+  Lemma qProject_None_inv_aux F T C l Ty G Q GG: 
+    GG = (rg_msg None F T C) ->
+    qProject GG Q -> 
+    F != T /\
+    (C l = Some (Ty, G) -> qProject G Q).
+  Proof.
+  move=> eq hp; punfold hp.
+  move: hp eq => [|p p' CONT {}Q neq hp |]//= [].
+  move=> eq1 eq2 eq3; split; [by rewrite -eq1 -eq2 | ].
+  rewrite -eq3; move=> conteq;  move: (hp _ _ _ conteq).
+  by rewrite /upaco2 /qProject /bot2; elim.
+  Qed.
+
+  Lemma qProject_None_inv F T C l Ty G Q (*Q'*):
+    qProject (rg_msg None F T C) Q -> 
+    F != T /\
+    (C l = Some (Ty, G) -> qProject G Q).
+  Proof.
+  by apply: qProject_None_inv_aux.
+  Qed.
+
+
+
+  Lemma qProject_Some_inv_aux l F T C Q GG: 
+    GG = (rg_msg (Some l) F T C) ->
+    qProject GG Q -> 
+    F != T /\
+    (exists Ty G Q',
+    C l = Some (Ty, G) /\
+    (*Q == (enq Q' (F,T) (l, Ty)) /\*)
+    deq Q (F, T) == Some ((l, Ty), Q') /\
+    qProject G Q').
+  Proof.
+  move=> eq hp; punfold hp.
+  move: hp eq  => [| | p p' CONT l0 Ty G {}Q Q' H0 H1 H2 H3] //= [].
+  move=> H4 H5 H6 H7. split; [by rewrite -H5 -H6 | exists  Ty, G, Q].
+  split; [by rewrite -H1 H7 H4 | split; [ by rewrite -H5 -H6 -H4|]].
+  by move: H3; rewrite /upaco2 /qProject /bot2; elim.
+  Qed.
+
+  Lemma qProject_Some_inv l F T C Q:
+    qProject (rg_msg (Some l) F T C) Q -> 
+    F != T /\
+    (exists Ty G Q', C l = Some (Ty, G) /\
+    (*Q == (enq Q' (F,T) (l, Ty)) /\*)
+    deq Q (F, T) == Some ((l, Ty), Q') /\
+    qProject G Q').
+  Proof.
+  by apply: qProject_Some_inv_aux.
+  Qed.
+
+  Definition deq_rinv 
+    p p' l Ty (Q: {fmap role * role -> seq (lbl * mty) }):=
+  match Q.[? (p,p')] with
+    | None => Q.[(p,p') <- ([::(l,Ty)])]
+    | Some q => Q.[(p,p') <- ( cons (l,Ty) q ) ]
+  end%fmap.
+
+
+ Lemma deq_rinv_deq p p' l Ty 
+    (Q: {fmap role * role -> seq (lbl * mty) }):
+    (*Q.[? (p,p')] != Some [::]*)
+    (forall q q', Q.[?(q,q')] != Some [::])->
+    deq (deq_rinv p p' l Ty Q) (p, p') = Some ((l, Ty), Q).
+  Proof.
+  move=> hp; rewrite /deq_rinv.
+  case E: Q.[? (p,p')] => [qs|]; rewrite /deq. 
+  + rewrite fnd_set; case: ifP =>//=; [|by rewrite eq_refl].
+    have noneq: qs != [::]. by move: (hp p p'); rewrite E.
+    move=> _; case: ifP.
+    * by rewrite -(rwP eqP); move=> eq; rewrite eq in noneq.
+    * move=> _; rewrite setfC; case: ifP =>//=; [|by rewrite eq_refl].
+      move=> _; apply f_equal; rewrite pair_equal_spec; split=>//=.
+      rewrite -fmapP; move=> k; rewrite fnd_set; case: ifP =>//=.
+      by rewrite -(rwP eqP); move=>->.
+  + rewrite fnd_set; case: ifP =>//=; [|by rewrite eq_refl].
+    move=> _; apply f_equal; rewrite pair_equal_spec; split=>//=.
+    rewrite -fmapP; move=> k; rewrite remf1_set.
+    case: ifP; [move=> _; rewrite fnd_rem1|by rewrite eq_refl].
+    case: ifP; [by []|rewrite (rwP negPf) -(rwP negPn) -(rwP eqP)].
+    by move=>->.
+  Qed.
+
+End QProjection.
