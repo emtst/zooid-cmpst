ef8b8e9e097c56d20c2ce3f38e509002c0b8749d
Francisco Ferreira <fferre16@ic.ac.uk>
2020-05-27 18:48:56 +0100

    started defining a trace operational semantics for processes

--- theories/Proc.v
+++ theories/Proc.v
@@ -47,6 +47,18 @@ with Alts : seq (lbl * (mty * l_ty)) -> Type :=
                    Alts ((l, (T, L)) :: a)
 .
 
+(* Unset Printing Notations. *)
+
+Print sigT.
+Fixpoint lookup_alt_cont T (a : seq (lbl * (mty * l_ty)))
+                                                   (* the existential is silly, it should be what a says *)
+          (l : lbl) (alts : Alts a): option (sigT (fun x => (coq_ty T -> Proc x))) :=
+  match alts with
+  (* | A_sing _ _ l' dproc => *)
+  (*   if l == l' then Some dproc else None *)
+  |_ => None
+  end.
+
 (* convenience definition for conditional processes *)
 Definition IFP L (n : bool) (p : Proc L) (p' : Proc L) :=
   if n then p else p'.
@@ -73,6 +85,30 @@ Module MP.
 
   Parameter rec : forall T1, nat -> t T1.
 End MP.
+
+(* Module MP'. *)
+(*   Parameter t : l_ty -> Type. *)
+
+(*   (* Parameter bind : forall T1 T2, t T1 -> (T1 -> t T2) -> t T2. *) *)
+(*   Parameter pure : t l_end. *)
+
+(*   Parameter send : forall (p : role) L a T (l : lbl), *)
+(*       coq_ty T -> (l, (T, L)) \in a -> *)
+(*       t (l_msg l_send p a). *)
+(*   (* Extract Constant send => "ocaml_send". *) *)
+
+(*   Print l_lts_. *)
+
+(*   (* Parameter recv : (lbl -> t unit) -> t unit. *) *)
+(*   (* Parameter recv_one : forall T, role -> t T. *) *)
+
+
+(*   (* Parameter loop : forall T1, nat -> t T1 -> t T1. *) *)
+(*   (* Parameter set_current: nat -> t unit. *) *)
+
+(*   (* Parameter rec : forall T1, nat -> t T1. *) *)
+(* End MP'. *)
+
 
 Fixpoint gen_proc T (n:nat) (p : Proc T): MP.t unit
   := match p with
@@ -107,8 +143,8 @@ with gen_alts (a :seq (lbl * (mty * l_ty)))
          gen_alts n r alts (new_f _ T dproc l)
        end.
 
-(* Definition p := Eval compute in gen_proc (Fix (Rec T_nat 0)). *)
-(* Extraction p. *)
+Print gen_proc.
+
 
 (* Some examples *)
 
@@ -250,6 +286,15 @@ Extraction ppc3.
 
 Section OperationalSemantics.
 
+  (* runtime action *)
+  Inductive rt_act :=
+  | mk_rt_act (a : l_act) (p : role) (q : role) (l : lbl) (T : mty) (t :  coq_ty T).
+
+  Definition erase_act a :=
+  let: mk_rt_act a p q l T _ := a in mk_act a p q l T.
+
+
+
 (*   SearchAbout rl_ty. *)
 
   Notation penv := {fmap role -> {T: l_ty & Proc T}}.
@@ -266,57 +311,45 @@ Section OperationalSemantics.
   (* Definition penv_eqMixin := EqMixin penv_eqP. *)
   (* Canonical penv_eqType := Eval hnf in EqType penv penv_eqMixin. *)
 
-  Definition pqenv := {fmap role * role -> seq (lbl * {T: mty & coq_ty T}) }.
+  (* Definition pqenv := {fmap role * role -> seq (lbl * {T: mty & coq_ty T}) }. *)
 
-  Definition eq_pqenv (Q Q' : pqenv) :=
-    match Q, Q' with
-    |  _, _ => false
-    end.
+  (* Definition eq_pqenv (Q Q' : pqenv) := *)
+  (*   match Q, Q' with *)
+  (*   |  _, _ => false *)
+  (*   end. *)
 
-  Lemma pqenv_eqP : Equality.axiom eq_pqenv.
-  (* Proof. by rewrite /Equality.axiom => [[] []/=]; constructor. Qed. *)
-  Admitted.
+  (* Lemma pqenv_eqP : Equality.axiom eq_pqenv. *)
+  (* (* Proof. by rewrite /Equality.axiom => [[] []/=]; constructor. Qed. *) *)
+  (* Admitted. *)
 
-  Definition pqenv_eqMixin := EqMixin pqenv_eqP.
-  Canonical pqenv_eqType := Eval hnf in EqType pqenv pqenv_eqMixin.
+  (* Definition pqenv_eqMixin := EqMixin pqenv_eqP. *)
+  (* Canonical pqenv_eqType := Eval hnf in EqType pqenv pqenv_eqMixin. *)
 
-  Definition penq (qs : pqenv) k v :=
-    match qs.[? k] with
-    | Some vs => qs.[ k <- app vs [:: v] ]
-    | None => qs.[ k <- [:: v]]
-    end%fmap.
+  (* Definition penq (qs : pqenv) k v := *)
+  (*   match qs.[? k] with *)
+  (*   | Some vs => qs.[ k <- app vs [:: v] ] *)
+  (*   | None => qs.[ k <- [:: v]] *)
+  (*   end%fmap. *)
 
-  Definition pdeq (qs : pqenv) k :=
-    match qs.[? k] with
-    | Some vs =>
-      match vs with
-      | [:: v] => Some (v, qs.[~ k])
-      | v :: vs => Some (v, qs.[k <- vs])
-        (* if vs == [::] then Some (v, qs.[~ k]) *)
-        (* else Some (v, qs.[k <- vs]) *)
-      | [::] => None
-      end
-    | None => None
-    end%fmap.
-
-  (* Unset Printing Notations. *)
+  (* Definition pdeq (qs : pqenv) k := *)
+  (*   match qs.[? k] with *)
+  (*   | Some vs => *)
+  (*     match vs with *)
+  (*     | [:: v] => Some (v, qs.[~ k]) *)
+  (*     | v :: vs => Some (v, qs.[k <- vs]) *)
+  (*       (* if vs == [::] then Some (v, qs.[~ k]) *) *)
+  (*       (* else Some (v, qs.[k <- vs]) *) *)
+  (*     | [::] => None *)
+  (*     end *)
+  (*   | None => None *)
+  (*   end%fmap. *)
 
   Definition process_admits_act
              T (P : Proc T) (a : l_act) (p q : role) (l : lbl) (t : mty) :
     option l_ty.
   Abort.
 
-  (* Definition penv_admits_act (P : penv) (a : l_act) (p q : role) (l : lbl) (t : mty) := *)
-    (* match P.[? p]  with *)
-    (* | Some (existT T Pi) =>  *)
-      (* match T with *)
-      (* (* | Send _ _ _ _ _ _ _ _ => None *) *)
-      (* | _ => None *)
-    (*   (* end *) *)
-    (* | None => None *)
-    (* end%fmap. *)
-
-  Inductive pstep : act -> penv * pqenv -> penv * pqenv -> Prop :=
+  (* Inductive pstep : act -> penv * pqenv -> penv * pqenv -> Prop := *)
   (* | ls_send (T : mty) (t : coq_ty T) p q lb (P P' : penv) (Q Q' : pqenv) : *)
   (*     Q' == penq Q (p, q) (lb, (existT _ T t)) -> *)
   (*     (* do_act P l_send p q lb t = Some P' -> *) *)
@@ -325,7 +358,8 @@ Section OperationalSemantics.
   (*     (* deq Q (p, q) == Some ((lb, t), Q') -> *) *)
   (*     (* do_act P l_recv q p lb t = Some P' -> *) *)
   (*     pstep (a_recv p q lb t) (P, Q) (P', Q') *)
-  .
+  (* . *)
+
 
   Definition look_proc (E : penv) p :=
     match E.[? p] with
@@ -333,27 +367,48 @@ Section OperationalSemantics.
     | None => existT _ l_end Finish
     end%fmap.
 
-  Definition do_act_proc (P : penv) (A : act) : option penv :=
-    let: (mk_act a p q l t) := A in
-    match look_proc P p with
-    | existT  _ (Send q' _ _ _ _ _ _ _) => None
-    | _ => None
+  Definition do_rt_act (P : penv) (A : rt_act) : option penv :=
+    let: (mk_rt_act a p q l t data) := A in
+    match a, look_proc P p with
+    | l_send, existT  _ (Send q' L _ t' l' T' P' _) =>
+      if (q == q') && (l == l') && (t == t') then (* && (T == T') then *) (* <- we may need to say this *)
+        Some P.[p <- existT _ L P']
+      else None
+    | l_recv, existT _ (Recv _ q' alts) =>
+      if q == q' then
+        match lookup_alt_cont t l alts with
+          | Some (existT L f) => Some P.[p <- (existT _ L (f data))]
+          | None => None
+        end
+      else None
+    | _, _ => None
     end%fmap.
 
-  Definition runnable_proc (A : act) (P : penv * pqenv) : bool :=
-    false
+
+  Definition run_rt_step (P : penv) (A : rt_act) : penv :=
+    match do_rt_act P A with
+    | Some P => P
+    | None => P
+    end.
+
+  Inductive pstep : rt_act -> penv -> penv -> Prop :=
+  | ls_send (Ti : mty) (t : coq_ty Ti)  (sact :rt_act) (P P' : penv) :
+      pstep sact P (run_rt_step P sact)
+  (* | ls_recv t p q lb (P P' : penv) (Q Q' : pqenv) : *)
+  (*     (* deq Q (p, q) == Some ((lb, t), Q') -> *) *)
+  (*     (* do_act P l_recv q p lb t = Some P' -> *) *)
+  (*     pstep (a_recv p q lb t) (P, Q) (P', Q') *)
   .
 
+  Lemma process_behave
+        (a : rt_act)
+        (PQ  PQ' : {fmap role -> rl_ty} * {fmap role * role -> seq (lbl * mty)})
+        (rP rP' : penv)
+    :
+      rP' = run_rt_step rP a ->
+      PQ' = run_step (erase_act a) PQ ->
 
-
-(*   Lemma process_behave *)
-(*         (a : act) *)
-(*         (P  P' : {fmap role -> rl_ty}) *)
-(*         (Q Q' : {fmap role * role -> seq (lbl * mty)}) *)
-(*         (rP rP' : {fmap role -> {T : l_ty & Proc T}}) *)
-(*         (rQ rQ' : {fmap role * role -> seq (lbl * {T : mty & coq_ty T})}) *)
-(*     : *)
-(*        pstep a (rP, rQ) (rP', rQ') -> lstep a (P, Q) (P', Q'). *)
-(* Abort. *)
+      pstep a rP rP' -> lstep (erase_act a) PQ PQ'.
+Abort.
 
 End OperationalSemantics.
