74eead41439c9d7a2dec336336cbde39c5dd11b1
Francisco Ferreira <fferre16@ic.ac.uk>
2020-06-23 16:34:34 +0100

    half nothing proof

--- theories/Global.v
+++ theories/Global.v
@@ -958,7 +958,7 @@ Section Semantics.
 
   Lemma g_lts_monotone : monotone2 g_lts_.
   Proof. pmonauto. Qed.
-  Hint Resolve g_lts_monotone.
+  Hint Resolve g_lts_monotone : paco.
 
   Close Scope mpst_scope.
 
