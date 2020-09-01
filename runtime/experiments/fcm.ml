a2d247ac5c79ee81553b064b986de2d2d9e66bf8
Francisco Ferreira <fferre16@ic.ac.uk>
2020-09-01 15:28:16 +0100

    example of the syntax of first class functors

--- runtime/experiments/fcm.ml
+++ runtime/experiments/fcm.ml
@@ -10,6 +10,21 @@ module Test : TEST =
     let f x = x
   end
 
+
+module type FUNCTOR_RESULT =
+  sig
+    val result : int
+  end
+
+module type TEST_FUNCTOR =
+  functor (T : TEST) -> FUNCTOR_RESULT
+
+
+module TestFunctor (T : TEST) =
+  struct
+    let result = T.f 8
+  end
+
 let real_f (x : int) : (module TEST) = (module Test)
 
 let real_f' (x : int) : (module TEST) =
@@ -18,3 +33,10 @@ let real_f' (x : int) : (module TEST) =
    end)
 
 let six = let (module M) = real_f' 5 in M.f 1
+
+
+let test_functor =
+  let m = real_f' 5 in
+  let (module M : TEST) = m in
+  let (module F : FUNCTOR_RESULT) = (module TestFunctor(M)) in
+  string_of_int F.result |> print_endline
