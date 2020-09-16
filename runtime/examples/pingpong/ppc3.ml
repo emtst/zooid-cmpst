853a29897e3d1431722f5596fbe492bfb87619ef
Francisco Ferreira <fferre16@ic.ac.uk>
2020-09-16 15:20:15 +0100

    added missinf files updated the gitignore file

---
+++ runtime/examples/pingpong/ppc3.ml
@@ -0,0 +1,14 @@
+open GeneratedLib.Code
+open Comm
+
+let pp_server = 0
+let pp_client = 1
+
+let participants = [
+    { role_to = pp_server
+    ; spec = Client(build_addr "127.0.0.1" 10001)
+  }]
+
+let () = print_endline "Implementation for the role of the client in pingpong"
+       ; if Log.create_log "ppc3" then print_endline "Logging." else ()
+       ; execute_extracted_process participants (module PPCLIENT3)
