e12463df29f64abb30ae6b97b41042194a0aafe5
David Castro-Perez <david.castro.dcp@gmail.com>
2021-02-26 13:42:21 +0000

    Scripts for building and saving docker image

--- docker/buildImage.sh
+++ docker/buildImage.sh
@@ -4,7 +4,15 @@ ZOOID_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; cd .. ; pwd -P )"
 
 pushd $ZOOID_DIR
 
+echo "WARNING!!"
+echo
+echo "About to run 'git clean -xdf'. Press ENTER if you are sure."
+read -r input
 
+git clean -xdf
+
+echo "Building docker image (requires 'sudo')"
+sudo docker build -t pldi21-paper92 -f docker/Dockerfile .
 
 popd
 
---
+++ docker/saveImage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker save pldi21-paper92:latest | gzip > pldi21-paper92.tar.gz
